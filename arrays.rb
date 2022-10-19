189c27b2f498de0314ca62f4873537623600b1ff
Tori Enyart <torienyart@gmail.com>
2022-10-19 13:46:45 -0600

    Call arrays and provide explanation

--- arrays.rb
+++ arrays.rb
@@ -6,3 +6,14 @@ floats = [2.3, 4.5, 7.3, 8.9]
 
 booleans = [true, false, true, true]
 
+#this line of code will call the 4th element in the enyarts array
+p enyarts(3)
+
+#this line of code will call the 4th element in the enyart_ages array
+p enyarts_ages(3)
+
+#this line of code will call the 1st element in the floats array
+p floats(0)
+
+#this line of code will call the 3rd element in the booleans array
+p booleans(2)
